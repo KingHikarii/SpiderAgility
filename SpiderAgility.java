0edb35db83c4828bcc48133ab010553c2d63122e
Degnan Kopa <103539937+KingHikarii@users.noreply.github.com>
2023-03-14 12:38:59 -0400

    Add files via upload

---
+++ SpiderAgility.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class SpiderAgility
+{
+    public static void main(String args[]){
+    Random rand_num = new Random();
+    int four = 4;
+    int limit_num = rand_num.nextInt(four);
+    System.out.println(limit_num);
+    }
+}

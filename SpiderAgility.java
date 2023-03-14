c167ef3c9c962237e64e8dbe1968aaea123981a1
Degnan Kopa <103539937+KingHikarii@users.noreply.github.com>
2023-03-14 12:55:12 -0400

    Add files via upload
    • Currently attempting to understand the base of the program, and trying to understand randomization.

--- SpiderAgility.java
+++ SpiderAgility.java
@@ -1,10 +1,25 @@
 import java.util.Random;
+import java.lang.Thread;
 public class SpiderAgility
 {
-    public static void main(String args[]){
-    Random rand_num = new Random();
+    public static void main(String args[]) throws InterruptedException{
+    Random rand_num = new Random();  //Put this in a class that will establish
     int four = 4;
-    int limit_num = rand_num.nextInt(four);
-    System.out.println(limit_num);
+    int limit_num;
+
+    int i = 0;
+
+    while (i == 0) //Put this in another class called: (Randomizer Number)
+    {
+        limit_num = rand_num.nextInt(four);
+        System.out.println(limit_num);
+        Thread.sleep(2000);
+        i++;
+
+        if (i == 5)
+        {
+            System.out.println("End.");
+        }
+    }
     }
 }

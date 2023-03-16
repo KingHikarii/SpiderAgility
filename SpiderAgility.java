d2989b01279be0ac78d8a52b830bf1be228dfa20
Degnan Kopa <103539937+KingHikarii@users.noreply.github.com>
2023-03-16 15:14:59 -0400

    Add files via upload

--- SpiderAgility.java
+++ SpiderAgility.java
@@ -8,18 +8,30 @@ import javax.swing.border.EmptyBorder;
 
 
 
-public class SpiderAgility {
+public class SpiderAgility extends JFrame{
+    private ImageIcon image1;
+    private JLabel label1;
     public void Application_Launcher() {
 
-            JFrame frame = new JFrame("My First GUI");
+            JFrame frame = new JFrame("Spider-Man's Agility");
             //"C:/Users/Yeshu/OneDrive/Desktop/Pictures and Videos/App_Spider"
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(300,300);
-            JButton button = new JButton("Press");
-            frame.getContentPane().add(button); // Adds Button to content pane of frame
+            frame.setSize(700,700);
+            frame.add(new JLabel(new ImageIcon((getClass().getResource("/images/App_Left.png")))));
+            //JButton button = new JButton("Press");
+            //frame.getContentPane().add(button); // Adds Button to content pane of frame
             frame.setVisible(true);
             frame.setIconImage(new ImageIcon(getClass().getResource("/images/App_Spider.png")).getImage());
+            //frame.add(new JLabel(new ImageIcon((getClass().getResource("/images/App_Left.png")))));
 
+        }
+        SpiderAgility()
+        {
+        setLayout(new FlowLayout());
+
+        image1 = new ImageIcon(getClass().getResource("/images/App_Down.png"));
+        label1 = new JLabel(image1);
+        add(label1);
         }
 
 
@@ -29,7 +41,11 @@ public class SpiderAgility {
     public static void main(String args[]) throws InterruptedException
     {
         SpiderAgility al = new SpiderAgility();
+        SpiderAgility al_2 = new SpiderAgility();
         al.Application_Launcher();
+        al_2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        al_2.setVisible(true);
+        al_2.pack();
 
         Random rand_num = new Random();  //Put this in a class that will establish
         int four = 4;
